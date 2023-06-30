86e337b38dde54f76229ecf795e8448178c24815
Maurane Lenoir <maurane1831@gmail.com>
2023-06-30 16:04:11 +0200

    30/06/23

--- js/exo1.js
+++ js/exo1.js
@@ -1,11 +1,22 @@
-const carre = document.createElement("div")
-carre.classList.add("carre")
 
-const content = document.querySelector("#content")
-content.appendChild(carre)
-carre.innerText = "Texte"
+
+// on veut que le message qui s'affiche dans l'alerte soit la class: carre et qu'il affiche son contenu
+
+const element = document.querySelector("#carre");
+const style = window.getComputedStyle(element);
 
 // Faire apparaître la boîte d'alerte
-carre.addEventListener("click", function() {
-    alert("")
-})
+function addAlert() {
+    alert(
+        "color : " +
+        style.color +
+        "\n background color: " +
+        style.backgroundColor +
+        "\n height: " +
+        style.height +
+        "n\ width: " +
+        style.width +
+        "n\ display: " +
+        style.display
+    );
+}
